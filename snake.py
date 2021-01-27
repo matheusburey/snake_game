38d395fb489fea07287ab0fc5c51581fe0ed628d
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-01-27 20:03:21 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -6,6 +6,9 @@ def on_grid_random():
     y = random.randint(0,590)
     return (x//10*10, y//10*10)
 
+def collision(c1,c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -42,6 +45,10 @@ while True:
                 my_direction = LEFT
             if event.key == K_RIGHT :
                 my_direction = RIGHT
+    
+    if collision(snake[0],apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
 
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
