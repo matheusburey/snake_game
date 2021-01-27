30abaa24d85df440f5f5ae58a32a3db336a4d573
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-01-26 22:04:01 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,14 +1,33 @@
-import pygame
+import pygame, random
 from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
 pygame.init()
 screen =pygame.display.set_mode((600,600))
 pygame.display.set_caption('snake')
+
+snake = [(200,200),(210,200),(220,200)]
+sn_skin = pygame.Surface((10,10))
+sn_skin.fill((255,255,255))
+my_direction = LEFT
+
+apple_pos = (random.randint(0,590),random.randint(0,590))
+apple = pygame.Surface((10,10))
+apple.fill((255,0,0))
 
 while True:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+
+    screen.fill((0,0,0))
+    screen.blit(apple,apple_pos)
+    for pos in snake:
+        screen.blit(sn_skin,pos)
 
     pygame.display.update()
