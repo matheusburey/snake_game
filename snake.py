84467e52c9e6fd4dea51b8966bbf670c79f4fec4
Matheus H. Burey <65559844+Matheus-Henrique-Burey@users.noreply.github.com>
2021-02-02 22:05:42 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -2,11 +2,11 @@ import pygame , random
 from pygame.locals import *
 
 def on_grid_random():
-    x = random.randint(0,590)
-    y = random.randint(0,590)
-    return (x//10*10, y//10*10)
+    x = random.randint(0,59)
+    y = random.randint(0,59)
+    return (x * 10 , y * 10)
 
-def collision(c1,c2):
+def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
 UP = 0
@@ -15,29 +15,30 @@ DOWN = 2
 LEFT = 3
 
 pygame .init()
-screen = pygame .display.set_mode((600,600))
-pygame .display.set_caption('snake')
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption('snake')
 
 snake = [(200, 200), (210,200),(220,200)]
-sn_skin = pygame .Surface((10,10))
+sn_skin = pygame.Surface((10,10))
 sn_skin.fill((255,255,255))
+
 apple_pos = on_grid_random()
-apple = pygame .Surface((10,10))
+apple = pygame.Surface((10,10))
 apple.fill((255,0,0))
 
 my_direction = LEFT
 
-clock = pygame .time.Clock()
+clock = pygame.time.Clock()
 
-font = pygame .font.Font('freesansbold.ttf', 18)
+font = pygame.font.Font('freesansbold.ttf', 18)
 score = 0
 
 game_over = False
 while not game_over:
-    clock.tick(20)
+    clock.tick(10)
     for event in pygame .event.get():
         if event.type == QUIT:
-            pygame .quit()
+            pygame.quit()
         
         if event.type == KEYDOWN:
             if event.key == K_UP :
@@ -49,12 +50,12 @@ while not game_over:
             if event.key == K_RIGHT :
                 my_direction = RIGHT
     
-    if collision(snake[0],apple_pos):
+    if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0,0))
         score = score + 1
 
-    if snake[0][0] == 600 or snake [0][1] == 600 or snake[0][0] <0 or snake[0][1] <0:
+    if snake[0][0] == 600 or snake [0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
         game_over = True
         break
 
@@ -66,12 +67,12 @@ while not game_over:
     if my_direction == DOWN:
         snake[0] = (snake[0][0], snake[0][1] + 10)
     if my_direction == RIGHT:
-        snake[0] = (snake[0][0]+10, snake[0][1])
+        snake[0] = (snake[0][0] +10, snake[0][1])
     if my_direction == LEFT:
-        snake[0] = (snake[0][0]-10, snake[0][1])
+        snake[0] = (snake[0][0] -10, snake[0][1])
 
     for i in range(len(snake)-1,0,-1):
-        snake[i] = snake[i-1][0], snake[i-1][1]
+        snake[i] = (snake[i-1][0], snake[i-1][1])
     
     screen.fill((0,0,0))
     screen.blit(apple,apple_pos)
@@ -90,7 +91,11 @@ while True:
     game_over_font = pygame.font.Font('freesansbold.ttf',75)
     game_over_screen = game_over_font.render('GAME OVER', True, (255,255,255))
     game_over_rect= game_over_screen.get_rect()
-    game_over_rect.midtop = (600 / 2, 10)
     screen.blit(game_over_screen, game_over_rect)
     pygame.display.update()
     pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit
