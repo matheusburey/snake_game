15e4e09b8700f030adf5c351c959ffd3f0ffe714
Matheus-Henrique-Burei <matheushburei.99@gmail.com>
2021-12-29 19:49:24 -0300

    refatoração da função de sesenhar elemntos nna tela

--- snake.py
+++ snake.py
@@ -4,12 +4,12 @@ from pygame.locals import *
 
 status = {
         'snake':{
-            'length': [(200, 200), (210, 200), (220, 200)],  # Cobra inicial
+            'position': [(200, 200), (210, 200), (220, 200)],  # Cobra inicial
             'pixel_size':pygame.Surface((10, 10)),  # Tamanho de cada quadrado da cobra
             'color_skin': ((255, 255, 255))  # cor da cobra: branca
         },
         'apple':{
-            'length': [(100, 100)],  # Calcula localização aleatoria
+            'position': [(100, 100)],  # Localização inicial da maçã
             'pixel_size': pygame.Surface((10, 10)),  # Tamanho da maçã
             'color_skin': ((255, 0, 0))   # Cor da maçã
             }
@@ -29,15 +29,9 @@ def collision(c1, c2):
 
 
 # Desenha a cobra na tela
-def print_snake(snake):
-    screen.fill((0, 12, 0))
-    for po in snake:
-        pygame.draw.rect(screen, status['snake']['color_skin'], [po[0], po[1], 10, 10])
-
-
-# Desenha a maçã na tela
-def print_aplle():
-        pygame.draw.rect(screen, red, [apple_pos[0], apple_pos[1], 10, 10])
+def print_element(element):
+    for po in element['position']:
+        pygame.draw.rect(screen, element['color_skin'], [po[0], po[1], 10, 10])
 
 
 #Verifica tecla clicada
@@ -72,12 +66,6 @@ def new_position(snake):
 pygame.init()  # inicio o pygame
 screen = pygame.display.set_mode((600, 600))  # Tamanho da tela
 pygame.display.set_caption('snake')  # Etiqueta do game
-
-# maça
-apple_pos = on_grid_random()  # Calcula localização aleatoria
-apple = pygame.Surface((10, 10))  # Tamanho da maçã
-red = ((255, 0, 0))  # Cor da maçã
-
 my_direction = 'LEFT'  # Direção inicial
 font = pygame.font.Font('freesansbold.ttf', 18)  # Fonte
 clock = pygame.time.Clock()  # Tempo de atualização da tela por segundo
@@ -87,34 +75,36 @@ score = 0   # Pontuaçao
 while True:
     pygame.display.update()
     clock.tick(10)
+    screen.fill((0, 12, 0))
     
-    print_snake(status['snake']['length'])
-    print_aplle()
+    print_element(status['snake'])
+    print_element(status['apple'])
 
     my_direction = click_key(pygame.event.get(), my_direction)
 
-    if collision(status['snake']['length'][0], apple_pos):
-        apple_pos = on_grid_random()
-        status['snake']['length'].append((0, 0))
+    if collision(status['snake']['position'][0], status['apple']['position'][0]):
+        status['apple']['position'][0] = on_grid_random()
+        status['snake']['position'].append((0, 0))
         score += 1
 
-    if status['snake']['length'][0][0] == 600 or status['snake']['length'][0][1] == 600 or status['snake']['length'][0][0] < 0 or status['snake']['length'][0][1] < 0:
+    if status['snake']['position'][0][0] == 600 or status['snake']['position'][0][1] == 600 or status['snake']['position'][0][0] < 0 or status['snake']['position'][0][1] < 0:
         break
 
-    if status['snake']['length'][0] in status['snake']['length'][1:]:
+    if status['snake']['position'][0] in status['snake']['position'][1:]:
         break
 
-    for i in range(len(status['snake']['length'])-1, 0, -1):
-        status['snake']['length'][i] = (status['snake']['length'][i-1][0], status['snake']['length'][i-1][1])
+    for i in range(len(status['snake']['position'])-1, 0, -1):
+        status['snake']['position'][i] = (status['snake']['position'][i-1][0], status['snake']['position'][i-1][1])
 
-    status['snake']['length'].pop()
+    status['snake']['position'].pop()
 
-    new_position(status['snake']['length'])
+    new_position(status['snake']['position'])
 
-    score_font = font.render('score: %s' % score, True, (155, 255, 255))
+    score_font = font.render(f'score: {score}', True, (155, 255, 255))
     score_rect = score_font.get_rect()
     score_rect.topleft = (600 - 120, 10)
     screen.blit(score_font, score_rect)
+
 
 while True:
     game_over_font = pygame.font.Font('freesansbold.ttf', 75)
